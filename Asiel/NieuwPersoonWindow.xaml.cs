b0cdf0bec7fbef395d22130338308b6000b9258c
lucschmeits <luc.06@live.nl>
2017-03-08 13:55:25 +0100

    functie vooer het toevoegen van personen aan listview

--- Asiel/NieuwPersoonWindow.xaml.cs
+++ Asiel/NieuwPersoonWindow.xaml.cs
@@ -19,14 +19,30 @@ namespace Asiel
     /// </summary>
     public partial class NieuwPersoonWindow : Window
     {
-        public NieuwPersoonWindow()
+        private List<Persoon> _persoonList;
+        private ListView _listViewPersonen;
+        public NieuwPersoonWindow(List<Persoon> persoonList, ListView listViewPersonen)
         {
             InitializeComponent();
+            _persoonList = persoonList;
+            _listViewPersonen = listViewPersonen;
         }
 
         private void btnOpslaan_Click(object sender, RoutedEventArgs e)
         {
+            Persoon p = new Persoon();
+            p.AddPersoon(TxtNaam.Text, TxtAchternaam.Text, TxtStraatHuisNr.Text, TxtPostcode.Text, TxtWoonplaats.Text, TxtTelefoon.Text);
+            _listViewPersonen.Items.Clear();
+            _persoonList.Add(p);
+            VulView();
+        }
 
+        private void VulView()
+        {
+            foreach (var p in _persoonList)
+            {
+                _listViewPersonen.Items.Add(p);
+            }
         }
     }
 }
