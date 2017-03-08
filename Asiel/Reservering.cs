bd95f494d95780b70c5c03ef7c91460f59983c14
lucschmeits <luc.06@live.nl>
2017-03-08 08:42:38 +0100

    Klassen reserving aangemaakt en properties toegevoegd

---
+++ Asiel/Reservering.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asiel
+{
+    class Reservering
+    {
+        private DateTime _reserveerDateTime;
+
+        public DateTime reserveerDateTime
+        {
+            get { return _reserveerDateTime; }
+            set { _reserveerDateTime = value; }
+        }
+
+        private bool _isOpgehaald;
+
+        public bool isOpgehaald
+        {
+            get { return _isOpgehaald; }
+            set { _isOpgehaald = value; }
+        }
+
+        private Persoon _persoon;
+
+        public Persoon persoon
+        {
+            get { return _persoon; }
+            set { _persoon = value; }
+        }
+
+        private Dier _dier;
+
+        public Dier dier
+        {
+            get { return _dier; }
+            set { _dier = value; }
+        }
+
+
+        public void ReserveerDier(Persoon p, Dier d, DateTime datum, List<Reservering> reserveringsList)
+        {
+            Reservering r = new Reservering
+            {
+                _persoon = p,
+                _dier = d,
+                _reserveerDateTime = datum
+            };
+            reserveringsList.Add(r);
+        }
+    }
+}
