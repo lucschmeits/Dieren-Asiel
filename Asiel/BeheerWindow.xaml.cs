4939fe04ee607e1e6d98617657f23c9a4ec1dd6d
lucschmeits <luc.06@live.nl>
2017-03-08 09:40:07 +0100

    Beheer window aangemaakt

--- Asiel/BeheerWindow.xaml.cs
+++ Asiel/BeheerWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Asiel
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for BeheerWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class BeheerWindow : Window
     {
-        public MainWindow()
+        public BeheerWindow()
         {
             InitializeComponent();
-        }
-
-        private void btnNieuwDier_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
