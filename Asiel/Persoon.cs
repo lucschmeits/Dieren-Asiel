194cd3fc6a38ada82e35587ea6eb602ad8812e99
lucschmeits <luc.06@live.nl>
2017-03-08 08:43:54 +0100

    klassen Persoon aangemaakt en properties toegevoegd

---
+++ Asiel/Persoon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asiel
+{
+    class Persoon
+    {
+        private string _naam;
+
+        public string naam
+        {
+            get { return _naam; }
+            set { _naam = value; }
+        }
+
+        private string _achternaam;
+
+        public string achternaam
+        {
+            get { return _achternaam; }
+            set { _achternaam = value; }
+        }
+
+        private string _straatHuisnr;
+
+        public string straatHuisnr
+        {
+            get { return _straatHuisnr; }
+            set { _straatHuisnr = value; }
+        }
+
+        private string _postcode;
+
+        public string postcode
+        {
+            get { return _postcode; }
+            set { _postcode = value; }
+        }
+
+        private string _woonplaats;
+
+        public string woonplaats
+        {
+            get { return _woonplaats; }
+            set { _woonplaats = value; }
+        }
+
+        private int _telefoonNr;
+
+        public int telefoonNr
+        {
+            get { return _telefoonNr; }
+            set { _telefoonNr = value; }
+        }
+
+
+        public void AddPersoon(string naamInput, string achternaamInput, string straatInput, string postcodeInput, string woonplaatsInput, int telefoonNrInput)
+        {
+            Persoon persoon = new Persoon
+            {
+                naam = naamInput,
+                achternaam = achternaamInput,
+                straatHuisnr = straatInput,
+                postcode = postcodeInput,
+                woonplaats = woonplaatsInput,
+                telefoonNr = telefoonNrInput
+            };
+        }
+    }
+}
