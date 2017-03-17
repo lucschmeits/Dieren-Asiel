944f287be592bb32d559672e620591a3bd3a3853
lucschmeits <luc.06@live.nl>
2017-03-17 13:30:14 +0100

    Unit test voor reserveringen toegevoegd

---
+++ TestProjectAsiel/UnitTestReserveren.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Asiel.Dieren;
+using Asiel;
+
+namespace TestProjectAsiel
+{
+    [TestClass]
+    public class UnitTestReserveren
+    {
+        private Reservering reservering;
+        private Persoon persoon;
+        private Dier dier;
+        [TestInitialize]
+        public void TestMethod1()
+        {
+            this.persoon = new Persoon("Kees", "Achternaam", "Stategaard 43", "6227 GL", "Maastricht", "0612345678");
+            this.dier = new Hond("Hondje", Dier.Geslacht.Man);
+            this.reservering = new Reservering(persoon, dier, DateTime.Today);
+        }
+
+        [TestMethod]
+        public void TestReserveringConstructor()
+        {
+            Assert.AreEqual("Kees", reservering.persoon.naam);
+        }
+    }
+}
