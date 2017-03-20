b5373325529ae3d131387060a38cdd04ab53851a
lucschmeits <luc.06@live.nl>
2017-03-20 16:45:45 +0100

    Interface ISellable toegevoegd

---
+++ Asiel/ISellable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asiel
+{
+    interface ISellable
+    {
+         string Naam { get; set; }
+         decimal Price { get; set; }
+    }
+}
